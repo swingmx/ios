d32442710824dde146fb903c8e270eda09ef55ba
panie18 <pauli@pauli.my>
2026-06-15 19:51:31 +0200

    .

--- SwingMusic/LiveActivity/RecentTrack.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-public struct RecentTrack: Codable, Hashable {
-    public let title: String
-    public let artist: String
-    public let imageData: Data?
-    public let accentHex: String
-}
